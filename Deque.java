0153a085eb8c9a3b7dcee381b3ae46161a1349ab
Revil <leowang137@gmail.com>
2013-09-08 01:11:19 +0800

    fix style error reported by checkstyle
    including tab-using, long-lines.

--- Deque.java
+++ Deque.java
@@ -2,145 +2,145 @@
 import java.util.Iterator;
 
 public class Deque<Item> implements Iterable<Item> {
-	
-	private Node sentinel;
-	private int  count;
-	
-	private class Node {
-		Item item;
-		Node prev;
-		Node next;
-	}
-	
-	// construct an empty deque
-	public Deque() {
-		sentinel = new Node();
-		sentinel.item = null;
-		sentinel.prev = sentinel;
-		sentinel.next = sentinel;
-		count = 0;
-	}
-	
+    
+    private Node sentinel;
+    private int  count;
+    
+    private class Node {
+        private Item item;
+        private Node prev;
+        private Node next;
+    }
+    
+    // construct an empty deque
+    public Deque() {
+        sentinel = new Node();
+        sentinel.item = null;
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        count = 0;
+    }
+    
     // is the deque empty?
-	public boolean isEmpty() {
-		return count == 0;
-	}
-	
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    
     // return the number of items on the deque
-	public int size() {
-		return count;
-	}
-	
+    public int size() {
+        return count;
+    }
+    
     // insert the item at the front
-	public void addFirst(Item item) {	
-		if (item == null)
-			throw new java.lang.NullPointerException("can't add a null item");
-		
-		Node front = new Node();
-		
-		front.item = item;
-		front.prev = sentinel;
-		front.next = sentinel.next;
-		
-		sentinel.next.prev = front;
-		sentinel.next = front;
-		count++;
-	}
-	   
-	// insert the item at the end
-	public void addLast(Item item) {
-		if (item == null)
-			throw new java.lang.NullPointerException("can't add a null item");
-		
-		Node last = new Node();
-		
-		last.item = item;
-		last.prev = sentinel.prev;
-		last.next = sentinel;
-		
-		sentinel.prev.next = last;
-		sentinel.prev = last;
-		count++;
-	}
-	
+    public void addFirst(Item item) {    
+        if (item == null)
+            throw new java.lang.NullPointerException("can't add a null item");
+        
+        Node front = new Node();
+        
+        front.item = item;
+        front.prev = sentinel;
+        front.next = sentinel.next;
+        
+        sentinel.next.prev = front;
+        sentinel.next = front;
+        count++;
+    }
+       
+    // insert the item at the end
+    public void addLast(Item item) {
+        if (item == null)
+            throw new java.lang.NullPointerException("can't add a null item");
+        
+        Node last = new Node();
+        
+        last.item = item;
+        last.prev = sentinel.prev;
+        last.next = sentinel;
+        
+        sentinel.prev.next = last;
+        sentinel.prev = last;
+        count++;
+    }
+    
     // delete and return the item at the front
-	public Item removeFirst() {
-		if (isEmpty())
-			throw new java.util.NoSuchElementException("Deque empty");
-		
-		Node first = sentinel.next;
-		Item fronItem = first.item;
-		
-		sentinel.next = first.next;
-		first.next.prev = sentinel;
-		
-		first = null;
-		count--;
-		return fronItem;
-	}
-	
+    public Item removeFirst() {
+        if (isEmpty())
+            throw new java.util.NoSuchElementException("Deque empty");
+        
+        Node first = sentinel.next;
+        Item fronItem = first.item;
+        
+        sentinel.next = first.next;
+        first.next.prev = sentinel;
+        
+        first = null;
+        count--;
+        return fronItem;
+    }
+    
     // delete and return the item at the end
-	public Item removeLast() {
-		if (isEmpty())
-			throw new java.util.NoSuchElementException("Deque empty");
-		
-		Node last = sentinel.prev;
-		Item endItem = last.item;
-		
-		sentinel.prev = last.prev;
-		last.prev.next = sentinel;
-		
-		last = null;
-		count--;
-		return endItem;
-	}
-	
-	// return an iterator over items in order from front to end
-	public Iterator<Item> iterator() { return new ListIterator(); }
-	
-	private class ListIterator implements Iterator<Item> {
-		private Node current = sentinel.next;
-		
-		public boolean hasNext() { return current != sentinel; }
-		public void remove() {
-			throw new java.lang.UnsupportedOperationException("remove operation currently not supported");
-		}
-		public Item next() {
-			if (current == sentinel)
-				throw new java.util.NoSuchElementException("No more items to return");
-			Item item = current.item;
-			current   = current.next;
-			return item;
-		}
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Deque<Integer> deque = new Deque<Integer>();
-		System.out.println(deque.isEmpty());
-		//deque.addFirst(5);
-		//deque.addLast(6);
-		System.out.println(deque.isEmpty());
-		//System.out.println(deque.removeLast());
-		System.out.println(deque.isEmpty());
-		System.out.println(deque.size());
-		//System.out.println(deque.removeFirst());
-		System.out.println(deque.isEmpty());
-		deque.addFirst(1);
-		deque.addFirst(2);
-		deque.addFirst(3);
-		deque.addFirst(4);
-		StdOut.println(deque.removeLast());
-		StdOut.println(deque.removeLast());
-		
-		deque.addLast(5);
-		StdOut.println(deque.removeFirst());
-		StdOut.println(deque.removeFirst());
-		StdOut.println(deque.removeFirst());
-		System.out.println(deque.size());
-		
-		for (Integer i : deque)
-			StdOut.println(i);
-	}
+    public Item removeLast() {
+        if (isEmpty())
+            throw new java.util.NoSuchElementException("Deque empty");
+        
+        Node last = sentinel.prev;
+        Item endItem = last.item;
+        
+        sentinel.prev = last.prev;
+        last.prev.next = sentinel;
+        
+        last = null;
+        count--;
+        return endItem;
+    }
+    
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator() { return new ListIterator(); }
+    
+    private class ListIterator implements Iterator<Item> {
+        private Node current = sentinel.next;
+        
+        public boolean hasNext() { return current != sentinel; }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+        public Item next() {
+            if (current == sentinel)
+                throw new java.util.NoSuchElementException();
+            Item item = current.item;
+            current   = current.next;
+            return item;
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Deque<Integer> deque = new Deque<Integer>();
+        System.out.println(deque.isEmpty());
+        //deque.addFirst(5);
+        //deque.addLast(6);
+        System.out.println(deque.isEmpty());
+        //System.out.println(deque.removeLast());
+        System.out.println(deque.isEmpty());
+        System.out.println(deque.size());
+        //System.out.println(deque.removeFirst());
+        System.out.println(deque.isEmpty());
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        StdOut.println(deque.removeLast());
+        StdOut.println(deque.removeLast());
+        
+        deque.addLast(5);
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeFirst());
+        System.out.println(deque.size());
+        
+        for (Integer i : deque)
+            StdOut.println(i);
+    }
 
 }
--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -3,111 +3,111 @@ import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	// just maintain one tail index
-	private int		tail;
-	private int		count;
-	private Item[]	q;
-	
+    // just maintain one tail index
+    private int        tail;
+    private int        count;
+    private Item[]    q;
+    
     // construct an empty randomized queue
-	public RandomizedQueue() {
-		q = (Item[]) new Object[1];
-		tail = 0;
-		count = 0;
-	}
-	
+    public RandomizedQueue() {
+        q = (Item[]) new Object[1];
+        tail = 0;
+        count = 0;
+    }
+    
     // is the queue empty?
-	public boolean isEmpty() { return count == 0; }
-	
+    public boolean isEmpty() { return count == 0; }
+    
     // return the number of items on the queue
-	public int size() { return count; }
-	
-	private void resize(int n) {
-		Item[] copy = (Item[]) new Object[n];
-		for (int i = 0; i < count; i++)
-			copy[i] = q[i];
-		q = copy;
-	}
-	
-	
-    // add the item	   
-	public void enqueue(Item item) {
-		if (item == null)
-			throw new java.lang.NullPointerException("null item not permitted");
-		
-		if (count == q.length) resize(2 * q.length);
-		q[tail++] = item;
-		count++;
-	}
-	
+    public int size() { return count; }
+    
+    private void resize(int n) {
+        Item[] copy = (Item[]) new Object[n];
+        for (int i = 0; i < count; i++)
+            copy[i] = q[i];
+        q = copy;
+    }
+    
+    
+    // add the item       
+    public void enqueue(Item item) {
+        if (item == null)
+            throw new java.lang.NullPointerException();
+        
+        if (count == q.length) resize(2 * q.length);
+        q[tail++] = item;
+        count++;
+    }
+    
     // delete and return a random item
-	public Item dequeue() {
-		if (isEmpty())
-			throw new java.util.NoSuchElementException("Queue empty");
-		
-		int index = StdRandom.uniform(tail);
-		Item item = q[index];
-		
-		// simply move the last item into the void
-		q[index]	= q[--tail];
-		q[tail] 	= null;
-		count--;
-		if (count > 0 && count == q.length / 4)
-			resize(q.length / 2);
-		
-		return item;
-	}
-	
+    public Item dequeue() {
+        if (isEmpty())
+            throw new java.util.NoSuchElementException();
+        
+        int index = StdRandom.uniform(tail);
+        Item item = q[index];
+        
+        // simply move the last item into the void
+        q[index]    = q[--tail];
+        q[tail]     = null;
+        count--;
+        if (count > 0 && count == q.length / 4)
+            resize(q.length / 2);
+        
+        return item;
+    }
+    
     // return (but do not delete) a random item
-	public Item sample() { return q[StdRandom.uniform(count)]; }
-	
-	// return an independent iterator over items in random order
-	public Iterator<Item> iterator() { return new ArrayIterator(); }
-	
-	private class ArrayIterator implements Iterator<Item> {
-		private int i = 0;
-		
-		public boolean hasNext() { return i < count; }
-		public void remove() {
-			throw new java.lang.UnsupportedOperationException("remove operation currently not supported");
-		}
-		public Item	next() {
-			if (i > count)
-				throw new java.util.NoSuchElementException("no more items");
-			return q[i++];
-		}
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
-		
-		StdOut.println(q.isEmpty());
-		q.enqueue(1);
-		q.enqueue(2);
-		q.enqueue(3);
-		q.enqueue(4);
-		q.enqueue(5);
-		q.enqueue(6);
-		q.enqueue(7);
-		StdOut.println(q.size());
-		
-		for (Integer i : q)
-			StdOut.print(i + " ");
-		StdOut.println();
-		
-		StdOut.println(q.dequeue());
-		
-		for (Integer i : q)
-			StdOut.print(i + " ");
-		StdOut.println();
-		
-		StdOut.println(q.dequeue());
-		StdOut.println(q.dequeue());
-		StdOut.println(q.size());
-		
-		for (Integer i : q)
-			StdOut.print(i + " ");
-		StdOut.println();
-	}
+    public Item sample() { return q[StdRandom.uniform(count)]; }
+    
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() { return new ArrayIterator(); }
+    
+    private class ArrayIterator implements Iterator<Item> {
+        private int i = 0;
+        
+        public boolean hasNext() { return i < count; }
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
+        public Item    next() {
+            if (i > count)
+                throw new java.util.NoSuchElementException();
+            return q[i++];
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        RandomizedQueue<Integer> q = new RandomizedQueue<Integer>();
+        
+        StdOut.println(q.isEmpty());
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.enqueue(6);
+        q.enqueue(7);
+        StdOut.println(q.size());
+        
+        for (Integer i : q)
+            StdOut.print(i + " ");
+        StdOut.println();
+        
+        StdOut.println(q.dequeue());
+        
+        for (Integer i : q)
+            StdOut.print(i + " ");
+        StdOut.println();
+        
+        StdOut.println(q.dequeue());
+        StdOut.println(q.dequeue());
+        StdOut.println(q.size());
+        
+        for (Integer i : q)
+            StdOut.print(i + " ");
+        StdOut.println();
+    }
 
 }
